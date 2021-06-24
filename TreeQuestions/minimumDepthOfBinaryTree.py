a0cf06b69c766fe7ad01ddb1dc25c5434b24c521
Brandon Dayauon <brdayauon@gmail.com>
2021-06-23 22:24:40 -0700

    with iterative solution (optimal)

--- TreeQuestions/minimumDepthOfBinaryTree.py
+++ TreeQuestions/minimumDepthOfBinaryTree.py
@@ -14,3 +14,35 @@ class Solution(object):
             return 0
         
         return helper(root)
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        depthQueue = deque([1])
+        queue = deque([root])
+        mn = float('inf')
+        while queue:
+            currNode = queue.popleft()
+            depth = depthQueue.popleft()
+            
+            if not currNode.left and not currNode.right:
+                mn = min(mn, depth)
+                break
+            
+            if currNode.left:
+                queue.append(currNode.left)
+                depthQueue.append(depth+1)
+            if currNode.right:
+                queue.append(currNode.right)
+                depthQueue.append(depth+1)
+            
+        return mn 
