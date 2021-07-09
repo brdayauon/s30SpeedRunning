f0fa20dacddb65a9bb6b896c6b8e419063acbe4a
Brandon Dayauon <brdayauon@gmail.com>
2021-07-08 22:05:31 -0700

    valid sudoku O(mn)

---
+++ HashProblems(WEEK2)/validSudoku.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        #go through the board and record placement as a string into a set
+        
+        seen = set()
+        rowSeen = set()
+        colSeen = set()
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == '.':
+                    continue
+                if (str(i//3) + str(board[i][j]) + str(j//3)) in seen:
+                    return False
+                if (str(i)+str(board[i][j])) in rowSeen:
+                    return False
+                if (str(j)+str(board[i][j])) in colSeen:
+                    return False
+                seen.add(str(i//3) + str(board[i][j]) + str(j//3))
+                rowSeen.add(str(i)+str(board[i][j]))
+                colSeen.add(str(j)+str(board[i][j]))
+
+        return True
