9cd47beaf7cb3e22b25fa2156d0a0d3001ffc002
Brandon Dayauon <brdayauon@gmail.com>
2021-07-20 22:28:05 -0700

    finished

---
+++ blind75/array/array/productOfArrayExceptSelf.py
@@ -0,0 +1,19 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+        
+        if not nums:
+            return res
+        
+        rp = 1
+        num = 1
+        for i in range(1,len(nums)):
+            rp = rp * nums[i-1]
+            res[i] = rp
+            
+        rp = 1
+        for i in range(len(nums)-2,-1,-1):
+            rp = rp * nums[i+1]
+            res[i] = rp * res[i]
+        
+        return res
