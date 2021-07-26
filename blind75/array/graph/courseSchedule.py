d3741e171fed42520b2d767b3f17bafc0ea36f76
Brandon Dayauon <brdayauon@gmail.com>
2021-07-25 22:28:34 -0700

    finished

---
+++ blind75/array/graph/courseSchedule.py
@@ -0,0 +1,38 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        #make adjacency list
+        indegree = [0] * numCourses
+        hm = {}
+        
+        #creating indegrees but putting subjects into indegree array. as well as put into hm 
+        for edge in prerequisites:
+            indegree[edge[0]] += 1
+            if edge[1] not in hm:
+                hm[edge[1]] = []
+            hm[edge[1]].append(edge[0])
+                
+                
+        queue = deque([])
+        #check for independent nodes
+        for i in range(len(indegree)):
+            if indegree[i] == 0:
+                queue.append(i)
+                
+        #process queue
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                curr = queue.popleft()
+                if curr in hm:
+                    edges = hm[curr]
+                    if edges:
+                        for edge in edges:
+                            indegree[edge] -= 1
+                            if indegree[edge] == 0:
+                                queue.append(edge)
+                                
+        for i in range(len(indegree)):
+            if indegree[i] != 0:
+                return False
+            
+        return True
