4126351613ffe654b3dfd65e9c082e100d34e26a
Brandon Dayauon <brdayauon@gmail.com>
2021-07-26 10:36:31 -0700

    redid with my own code w/ drawn out solution

--- graph/eventualSafeNodes.py
+++ graph/eventualSafeNodes.py
@@ -1,29 +1,77 @@
+#DFS
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        degree = [0 for i in range(len(graph))]
-        res = deque([])
-        queue = deque([])
+        hm = {}
         
-        #need in degree
-        from collections import defaultdict as dd
-        hm = dd(set)
+        #create adjacency list .. of indegree? { 0 : 3} 3 points to 0
         for i in range(len(graph)):
             for j in range(len(graph[i])):
-                hm[graph[i][j]].add(i)
-        
+                if graph[i][j] not in hm:
+                    hm[graph[i][j]] = []
+                hm[graph[i][j]].append(i)
+                
+        degree = [0 for i in range(len(graph))]
         for i in range(len(graph)):
             for j in range(len(graph[i])):
                 degree[i] += 1
-            
+        
+        queue = deque([])
+        stack = []
+        for i in range(len(degree)):
+            if degree[i] == 0:
+                stack.append(i)
+        
+        while stack:
+            curr = stack.pop() 
+            if curr in hm:
+                neigh = hm[curr]
+
+                for n in neigh:
+                    degree[n] -= 1
+                    if degree[n] == 0:
+                        stack.append(n)
+        res = []
+        for i in range(len(degree)):
+            if degree[i] == 0:
+                res.append(i)
+        return res
+
+
+
+#BFS
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        hm = {}
+        
+        #create adjacency list .. of indegree? { 0 : 3} 3 points to 0
+        for i in range(len(graph)):
+            for j in range(len(graph[i])):
+                if graph[i][j] not in hm:
+                    hm[graph[i][j]] = []
+                hm[graph[i][j]].append(i)
+                
+        degree = [0 for i in range(len(graph))]
+        for i in range(len(graph)):
+            for j in range(len(graph[i])):
+                degree[i] += 1
+        
+        queue = deque([])
+    
+        for i in range(len(degree)):
             if degree[i] == 0:
                 queue.append(i)
-                
+        
         while queue:
-            node = queue.popleft()
-            res.append(node)
-            for neighbor in hm[node]:
-                degree[neighbor] -= 1
-                if degree[neighbor] == 0:
-                    queue.append(neighbor)
-        return sorted(res)
-         
+            curr = queue.popleft() 
+            if curr in hm:
+                neigh = hm[curr]
+
+                for n in neigh:
+                    degree[n] -= 1
+                    if degree[n] == 0:
+                        queue.append(n)
+        res = []
+        for i in range(len(degree)):
+            if degree[i] == 0:
+                res.append(i)
+        return res
