005b3cb08cd22522efa06ad89ea367feb354fd6c
Brandon Dayauon <brdayauon@gmail.com>
2021-07-30 12:48:57 -0700

    finished

--- Heap/KClostPointsToOrigin.py
+++ Heap/KClostPointsToOrigin.py
@@ -16,3 +16,22 @@ class Solution:
           res.append(coordinates)
 
         return res
+
+  #updated and faster
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        heap = []
+        for point in points:
+            x = point[0]*point[0]
+            y = point[1]*point[1]
+            distance = math.sqrt(x+y)
+            heapq.heappush(heap, (-distance, (point[0],point[1])))
+            
+            if len(heap) > k:
+                heapq.heappop(heap)
+        res = []        
+        while heap:
+            dist, point = heapq.heappop(heap)
+            res.append(point)
+            
+        return res
