19e00f032f92d4831a4e466d577d0a2451628866
Brandon Dayauon <brdayauon@gmail.com>
2021-07-31 18:28:37 -0700

    included recursive solution

--- dynamic programming/wordBreak.py
+++ dynamic programming/wordBreak.py
@@ -13,3 +13,33 @@ class Solution:
                     dp[i] = True
                     break
         return dp[-1]
+
+
+"""
+U - 
+apple|pen|apple ["apple","pen"]
+cats|and|og ["cats","dog","sand","and","cat"]
+cat|sand|og
+​
+M- recursive --> dp
+P- go through string
+    check if its in wordDict (valid word)
+    
+"""
+​
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        if not s:
+            return False
+        self.seen = set(wordDict)
+        return self.helper(s)
+    
+    def helper(self, s):
+        #base 
+        if len(s) == 0:
+            return True
+        #logic
+        for i in range(len(s)):
+            if s[0:i+1] in self.seen and self.helper(s[i+1:]):
+                return True
+        return False
