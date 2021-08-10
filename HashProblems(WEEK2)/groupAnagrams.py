7eeb1b9683852183c5da4d681ae1bdf686e4806d
Brandon Dayauon <brdayauon@gmail.com>
2021-08-09 22:48:54 -0700

    updated

--- HashProblems(WEEK2)/groupAnagrams.py
+++ HashProblems(WEEK2)/groupAnagrams.py
@@ -17,3 +17,19 @@ class Solution:
             res.append(val)
             
         return res
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hm = {}
+        
+        for i in range(len(strs)):
+            temp = sorted(strs[i])
+            temp = ''.join(temp)
+            if temp not in hm:
+                hm[temp] = [strs[i]]
+            else:
+                hm[temp].append(strs[i])
+        res = []
+        for key,val in hm.items():
+            res.append(val)
+        return res
