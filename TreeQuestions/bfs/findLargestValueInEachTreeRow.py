9a6391c306644d220f5be00a33b1aabd99b3b078
Brandon Dayauon <brdayauon@gmail.com>
2021-08-13 21:27:25 -0700

    asd

---
+++ TreeQuestions/bfs/findLargestValueInEachTreeRow.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def largestValues(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        
+        queue = deque([root])
+        res = []
+        
+        while queue:
+            size = len(queue)
+            mx = float('-inf')
+            for i in range(size):
+                curr = queue.popleft()
+                
+                mx = max(mx, curr.val)
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+            res.append(mx)
+        return res
--- TreeQuestions/bfs/maxDepthBinaryTree.py
+++ TreeQuestions/bfs/maxDepthBinaryTree.py
@@ -21,3 +21,27 @@ class Solution:
                 stack.append((depth+1, curr.right))
                 
         return mx
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        queue = deque([(root)])
+        mx = 0
+        while queue:
+            size = len(queue)
+            mx += 1
+            for i in range(size):
+                curr = queue.popleft()
+                if curr.left:
+                    queue.append((curr.left))
+                if curr.right:
+                    queue.append((curr.right))
+        return mx
