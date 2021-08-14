53e3abe027f70188143204e618e1c2cc86c7aeba
Brandon Dayauon <brdayauon@gmail.com>
2021-08-13 21:31:36 -0700

    updated

--- dfs/employeeImportance.py
+++ dfs/employeeImportance.py
@@ -59,3 +59,32 @@ class Solution:
                 for person in employees:
                     stack.append(person)
         return total
+
+"""
+ANOTHER SOLUTION FROM THE DOME
+"""
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        hm = {}
+        total = 0
+        stack = [id]
+        for employee in employees:
+            hm[employee.id] = employee
+        
+        while stack:
+            person = stack.pop()
+            employee = hm[person]
+            total += employee.importance 
+            for neighbors in (employee.subordinates):
+                stack.append(neighbors)
+                
+        return total
