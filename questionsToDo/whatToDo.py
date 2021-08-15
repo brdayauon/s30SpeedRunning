8eaf902f42d5c798319a09b3d0fcadebae950355
Brandon Dayauon <brdayauon@gmail.com>
2021-08-14 18:19:29 -0700

    updated

--- questionsToDo/whatToDo.py
+++ questionsToDo/whatToDo.py
@@ -1,11 +1,20 @@
-# # june 20, 2021
-# # 1584. Min Cost to Connect All Points
+# # # june 20, 2021
+# # # 1584. Min Cost to Connect All Points
+# # # https://leetcode.com/problems/min-cost-to-connect-all-points/
+
+
 # # https://leetcode.com/problems/min-cost-to-connect-all-points/
 
-
-# https://leetcode.com/problems/min-cost-to-connect-all-points/
-
-https://leetcode.com/problems/concatenated-words/
+# https://leetcode.com/problems/concatenated-words/
 
 
-https://leetcode.com/problems/count-number-of-nice-subarrays/
+# https://leetcode.com/problems/count-number-of-nice-subarrays/
+
+
+# https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/
+
+# https://leetcode.com/problems/path-with-maximum-gold/
+
+# https://hackmd.io/@2aDFKMu8S2CUg05JnLuKKg/rkV803Vgw
+
+# https://leetcode.com/problems/connecting-cities-with-minimum-cost/
