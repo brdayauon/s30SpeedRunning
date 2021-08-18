5cb5d13299d7ee1cdc0e9b3a0dcb27c3eafdc396
Brandon Dayauon <brdayauon@gmail.com>
2021-08-17 18:06:28 -0700

    finished

---
+++ stack/minAddToMakeParenthesisValid.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        stack = []
+        count = 0
+        for i in range(len(s)):
+            c = s[i]
+            if c == '(':
+                stack.append('(')
+            elif stack and c == ')':
+                stack.pop()
+            else:
+                count += 1
+        return count + len(stack)
