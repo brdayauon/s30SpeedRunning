ebae08bdba5790cbf40bd2b0b265b0c352aae6e0
Brandon Dayauon <brdayauon@gmail.com>
2021-08-26 23:37:40 -0700

    finished

---
+++ careerQuestions/codesignal8.26.py
@@ -0,0 +1,16 @@
+# oncat edge letters: given an array string, output the result. 
+# for example:
+# input = [dog, cat, cow]
+# output = ['dt','cw',cg'] 
+# dt: because beginining of dog and t because end of cat.
+# cw: because beginning of cat and w because end of cow
+# cg: because beginning of cow and g because end of dog 
+# reduceTheNumber: given array, and k as parameters, return the string.
+# input = ["1111122222"]
+# output = ["12312"] i think?
+# given an array of digits, find out how many pairs have the same bits. 
+# input: [3,5,1,7,8,10,62,13]
+# output: 5
+# because:
+# bitArray: [2,2,1,3,1,2,5,3]
+# basically find the pairs in bit array
