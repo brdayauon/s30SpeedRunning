239e85d24b1b2d64cc7905e309a2482b854c5354
Brandon Dayauon <brdayauon@gmail.com>
2021-10-27 11:17:58 -0700

    finished

---
+++ graph/graphExploreCardLC/BFSAlgInGraph/NAryTreeLevelOrderTraversal.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        
+        queue = deque([root])
+        res = []
+        while queue:
+            size = len(queue)
+            temp = []
+            for i in range(size):
+                curr = queue.popleft()
+                
+                temp.append(curr.val)
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+                    
+            res.append(temp)
+            
+        return res
